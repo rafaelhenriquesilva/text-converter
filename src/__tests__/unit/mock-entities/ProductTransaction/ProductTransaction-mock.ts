24914df99005e88f3332886323850b618ca14f50
Rafael Silva <rafaelhenriquesilva494@gmail.com>
2024-08-03 15:07:25 -0300

    refactor mock entity product transaction

--- src/__tests__/unit/mock-entities/ProductTransaction/ProductTransaction-mock.ts
+++ src/__tests__/unit/mock-entities/ProductTransaction/ProductTransaction-mock.ts
@@ -9,14 +9,23 @@ export const createProductTransactionMock = () => {
       min: 5,
       max: 20
     }), 
-    idProduct: '70', 
+    idProduct: faker.number.int({
+      min: 10,
+      max:50
+    }).toString(), 
     productValue: 70.7, 
     createdAt: new Date(), 
     transactionDate: '2024-08-01', 
     updatedAt: new Date(), 
-    idUser: '5', 
+    idUser: faker.number.int({
+      min: 10,
+      max:50
+    }).toString(), 
     id: faker.string.uuid(), 
-    idOrder: '753', 
+    idOrder: faker.number.int({
+      min: 10,
+      max:50
+    }).toString(), 
  
   }) 
 }
